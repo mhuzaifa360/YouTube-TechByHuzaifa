382122ab8d3d5a4a3dcb496b3e2036eafd4e92a4
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2025-02-09 17:22:03 +0500

    rename the folder to master web development course

---
+++ Full javascript in one video/app.js
@@ -0,0 +1,60 @@
+// javascript course
+
+// console.log
+console.log("heloo and welcome to final paper")
+console.log("ye js ka conosle hai")
+
+// document.write()
+document.write("hello and welcome to techbyhuzaifa")
+
+// alert
+// alert('huzaifa');
+// alert('input some text here!!')
+
+// innerHTML
+document.getElementById('heading -').innerHTML = "heading 2";
+
+document.getElementById('myP').innerHTML = document.getElementById('heading').innerHTML;
+
+//variables in js
+// var, const, let
+var name = "ali"
+var firstName = "Muhammad"
+var lastName = " huzaifa"
+console.log(firstName + lastName)
+console.log("my name is " + name);
+// alert("my name is " + firstName + lastName)
+
+//window.prompt
+// var name1 = prompt("Enter your name !!!")
+// document.write(name1)
+
+//simple calculation
+var num1 = 12;
+var num2 = 10;
+console.log('The addition of num1 and num2 is ' + (num1+num2))
+console.log('the subtraction of num1 and num2 ' + (num1-num2));
+
+// data type
+var name2 = 'salman'
+var rollNo = 122;
+var isORNot = true;
+var hello;
+
+console.log(name2+ " "+ rollNo +" "+ isORNot + " " + hello);
+
+// array
+var names = ['huzaifa','ali','hamza','abdullah']
+console.log(names);
+
+//objects
+var student = {
+    name: 'ali',
+    rollNo: 23,
+    check: false
+}
+console.log(student);
+console.log(student.name);
+console.log(student.rollNo);
+
+
